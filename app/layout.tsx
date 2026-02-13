b1af90ee07081e5a3d108e887b21515b1e91ca63
StanislavOzarchuk <dr.ozarchuks@gmail.com>
2026-02-13 02:37:41 +0100

    fix: add Telegram WebApp SDK script to layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script src="https://telegram.org/js/telegram-web-app.js" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
